d4d75d18ba8ce9eadfd7a26fb029a8347b38e4da
Adrian Booth <Adrian.booth17@gmail.com>
2016-01-03 00:37:15 +0000

    Bulls and cows now recorded, but test stub not working

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,58 @@
 class Game
 
-  attr_reader :player_number
+  attr_accessor :player_number, :comp_number, :cows, :bulls
 
   def initialize
-    @player_number
+    @player_number = []
+    @comp_number = []
+    @cows = 0
+    @bulls = 0
   end
 
 
   def player_choice(number)
-    @player_number = number
+    @player_number = number.to_s.split('').map(&:to_i)
+    has_cows?
+    has_bulls?
+  end
+
+  def has_bulls?
+    @player_number.each_with_index do |x,i|
+      if @player_number[i] == @comp_number[i]
+        @bulls+=1
+      end
+    end
+    @bulls
+  end
+
+  def comp_choice
+    number = (0...4).map { |i| rand((i == 0 ? 1 : 0)..9) }.join.to_i
+    @comp_number = number.to_s.split('').map(&:to_i)
+  end
+
+  # private
+
+  def has_cows?
+    @player_number.each do |x|
+      if @comp_number.include?(x)
+        @cows+=1
+      end
+    end
+    @cows
   end
 
 end
+
+# num = 1234
+
+# print num.to_s.split('').map(&:to_i)
+#
+# game = Game.new
+#
+# print game.comp_choice
+#
+# game.player_choice(1234)
+#
+# print game.player_number
+#
+# print game.bulls
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,9 +3,29 @@ require 'game'
 
     let(:game) { Game.new }
 
-    it 'should allow the player to store a 4 digit number' do
+    it 'should allow the computer to generate a random 4 digit number' do
+      allow(game).to receive(:comp_choice).and_return 5678
+      expect(game.comp_choice).to eq(5678)
+    end
+
+    it 'should allow the player to chose and score no cows' do
+      allow(game).to receive(:comp_choice).and_return 5678
       game.player_choice(1234)
-      expect(game.player_number).to eq(1234)
+      expect(game.cows).to eq(0)
+    end
+
+    it 'should allow the player to chose and score 3 cows' do
+      allow(game).to receive(:comp_choice).and_return([5,6,7,8])
+      subject.player_choice(8562)
+      p subject.player_number
+      p subject.comp_number
+      expect(subject.cows).to eq(3)
+    end
+
+    it 'should allow the player to chose and score 2 bulls if they chose right index' do
+      allow(game).to receive(:comp_choice).and_return([5,6,7,8])
+      game.player_choice(5612)
+      expect(game.bulls).to eq(2)
     end
 
   end
