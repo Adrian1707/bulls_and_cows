870ab7e4b1b92b4d592786e9e5acbbe2f124359a
Andrew Stelmach <andrew_stelmach@hotmail.com>
2016-01-14 03:06:37 +0000

    passes the test and doesn't crash, but not sure if it's working as it should

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,13 +10,14 @@ class StaticPagesController < ApplicationController
     game = Game.new
     @comp_choice = game.comp_choice
     @comp_number = game.comp_number
-    session[:game] = game
     @result = session[:result]
-    @comp_number = session[:game].comp_number
+    @comp_number = game.comp_number
+    session[:game] = game.to_yaml
   end
 
   def save_number
-    session[:result] = session[:game].player_choice(params[:number])
-    redirect_to '/play_game'
+    game = YAML::load(session[:game])
+    session[:result] = game.player_choice(params[:number])
+    redirect_to game_path
   end
 end
